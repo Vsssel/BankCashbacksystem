ee318db344456ff36dbb2183219d15ba1a12f3fd
Mathew Z <220107138@stu.sdu.edu.kz>
2024-04-13 14:25:43 +0500

    deleted components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css';
-import AppCard from './shared/AppCard'; 
 
 function App() {
-  const appData = {
-    name: 'Hello World',
-    description: 'first time use'
-  };
 
   return (
-    <AppCard app={appData}></AppCard>
+    <></>
   );
 }
 
